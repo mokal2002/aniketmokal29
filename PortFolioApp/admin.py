10be0e116391b8446d6647a51f2db5b2290f4947
Aniket Mokal <79739328+mokal2002@users.noreply.github.com>
2024-02-07 18:08:05 +0530

    Delete PortFolioApp directory

--- PortFolioApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Contact)
-admin.site.register(MySkills)
-admin.site.register(MyPortfolio)
-admin.site.register(Feedback)
--- PortFolioApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortfolioappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "PortFolioApp"
